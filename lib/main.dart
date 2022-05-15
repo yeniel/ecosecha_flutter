29dacad3acb0d91a7a7d480a49473fdff352443c
Yeniel Landestoy <yeniel.landestoy.mindapps@gmail.com>
2022-05-15 02:22:29 +0200

    Init shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
+import 'dart:async';
+
 import 'package:ecosecha_flutter/app.dart';
 import 'package:ecosecha_flutter/data/data.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'injectable.dart';
 
-void main() {
-  var apiClient = HttpApiClient();
-  var authService = AuthService(apiClient: apiClient);
+void main() async {
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await SharedPreferences.getInstance();
 
-  configureDependencies();
-  runApp(App(
-    authService: authService,
-    repository: Repository(apiClient: apiClient, authService: authService),
-  ));
+      var apiClient = HttpApiClient();
+      var authService = AuthService(apiClient: apiClient);
+
+      configureDependencies();
+      runApp(App(
+        authService: authService,
+        repository: Repository(apiClient: apiClient, authService: authService),
+      ));
+    },
+    (error, st) => print(error),
+  );
 }
